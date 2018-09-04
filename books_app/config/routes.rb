55341a379d0e3856e80bf59442f715e5bc8a883b
lime1024 <lime1024@gmail.com>
2018-09-04 17:37:51 +0900

    Add devise gem
    ユーザ認証の為にdeviseを追加及び、それに伴いi18n対応

--- books_app/config/routes.rb
+++ books_app/config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  devise_for :users, controllers: {
+    confirmations: 'users/confirmations',
+    passwords:     'users/passwords',
+    registrations: 'users/registrations',
+    sessions:      'users/sessions',
+  }
+  
   resources :books
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
