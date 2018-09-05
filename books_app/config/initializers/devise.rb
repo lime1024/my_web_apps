3fe8320fe1b5fa00929fe4954166c29413c96c0c
lime1024 <lime1024@gmail.com>
2018-09-05 16:12:24 +0900

    Remove rubocop offences, test file and devise-i18n-views gem
    Rubocopのチェック、不要なテストファイルの削除と不要gemの削除

--- books_app/config/initializers/devise.rb
+++ books_app/config/initializers/devise.rb
@@ -9,7 +9,7 @@ Devise.setup do |config|
   # Devise will use the `secret_key_base` as its `secret_key`
   # by default. You can change it below and use your own secret key.
   # config.secret_key = 'c875fa02dc25eb7c6f642bc2fd2d190bc7931115334a4546527b8606efbd8af5aa3592cde434d422addbf7fa83007c499f1a1f5652e37a165fe3171eeef4e44f'
-  
+
   # ==> Controller configuration
   # Configure the parent class to the devise controllers.
   # config.parent_controller = 'DeviseController'
@@ -18,7 +18,7 @@ Devise.setup do |config|
   # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class
   # with default "from" parameter.
-  config.mailer_sender = 'please-change-me-at-config-initializers-devise@example.com'
+  config.mailer_sender = "please-change-me-at-config-initializers-devise@example.com"
 
   # Configure the class responsible to send e-mails.
   # config.mailer = 'Devise::Mailer'
@@ -30,7 +30,7 @@ Devise.setup do |config|
   # Load and configure the ORM. Supports :active_record (default) and
   # :mongoid (bson_ext recommended) by default. Other ORMs may be
   # available as additional gems.
-  require 'devise/orm/active_record'
+  require "devise/orm/active_record"
 
   # ==> Configuration for any authentication mechanism
   # Configure which keys are used when authenticating a user. The default is
