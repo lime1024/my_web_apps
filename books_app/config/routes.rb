c9d4ed18a78608053e80190b05912c85c9372688
lime1024 <lime1024@gmail.com>
2018-09-19 15:42:30 +0900

    Add omniauth

--- books_app/config/routes.rb
+++ books_app/config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
     passwords:     "users/passwords",
     registrations: "users/registrations",
     sessions:      "users/sessions",
+    omniauth_callbacks: "users/omniauth_callbacks"
   }
   root "books#index"
   resources :user, only: [:new, :create]
